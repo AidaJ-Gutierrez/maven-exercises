63aacc4d3fd634a1e84c8691a928c79f3a7b82bf
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-01-20 16:32:24 -0600

    dependency exercise finished

--- src/main/java/DependencyExercise.java
+++ src/main/java/DependencyExercise.java
@@ -8,19 +8,20 @@ public class DependencyExercise {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter something:");
         String userInput = sc.nextLine();
-        System.out.printf("You entered: %d ", userInput );
+        System.out.println("You entered:  " + userInput );
 
+        // checks if input is number: StringUtils.isNumeric
         if (StringUtils.isNumeric(userInput)){
-            System.out.printf("\n\"%s\" is a number\n", userInput);
-            System.out.println("Flipped Case: Not Applicable");
+            System.out.printf( userInput + " is a number\n");
+            System.out.println("Flipped Case: Not able to flip");
         }else{
             System.out.printf("\n\"%s\" is not a number\n", userInput);
-            //flips the case of the string
+            //flips the case of the string: StringUtils.swapCase
             System.out.println("Flipped Case: " + StringUtils.swapCase(userInput));
         }
 
         //reverses the string
-        System.out.println("Reversed: " + StringUtils.reverse(userInput));
+        System.out.println("Reversed Case: " + StringUtils.reverse(userInput));
 
     }
 
